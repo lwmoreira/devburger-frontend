a06137c13c33c194306907bebf427c7e4a249ad6
lwmoreira <lwmoreira01@gmail.com>
2024-08-21 20:41:30 -0300

    continuando correção de imagens

--- src/components/OffersCarousel/index.js
+++ src/components/OffersCarousel/index.js
@@ -48,7 +48,7 @@ export function OffersCarousel() {
           offers.map(products => (
             <ContainerItens key={products.id}>
               <Image
-                src={`https://devburger-backend-production.up.railway.app/${products.path}`}
+                src={`https://devburger-backend-production.up.railway.app/product-file/${products.path}`}
                 alt={`imagem do produto ${products.name}`}
               />
               <p>{products.name}</p>
