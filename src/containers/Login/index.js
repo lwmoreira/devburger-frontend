6f3fadcc5c03254794905317c8728e099a017b24
lwmoreira <lwmoreira01@gmail.com>
2024-08-20 23:03:02 -0300

    corrigindo caminhos

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -55,7 +55,7 @@ export function Login() {
       setTimeout(() => {
         if (data.admin) {
           navigate('/')
-        } else navigate('/')
+        } else navigate('/login')
       }, 1000)
     } catch (error) {
       if (error.response && error.response.status === 401) {
